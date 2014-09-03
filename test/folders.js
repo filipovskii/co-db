bab1a5f01559ad101efe98b2098977d6a6d89e3d
filipovsky <filipovskii.off@gmail.com>
2014-09-03 22:40:28 +0400

    Tets structure.

--- test/folders.js
+++ test/folders.js
@@ -4,63 +4,72 @@ var assert = require('assert'),
     co = require('co');
 
 
-describe('db nesting', function () {
+describe('db', function () {
 
   beforeEach(function () {
     fs.mkdirpSync('test/nested-db/folder1');
     fs.mkdirpSync('test/nested-db/folder2');
   });
 
+  describe('.dbs()', function () {
 
-  it('db.dbs() lists inner folders', function (done) {
-    co(function *() {
-      var db = yield docdb('test/nested-db'),
-          innerDbs;
+    it('lists inner folders', function (done) {
+      co(function *() {
+        var db = yield docdb('test/nested-db'),
+            innerDbs;
 
-      innerDbs = yield db.dbs();
-      assert.equal(innerDbs.length, 2);
-      assert.equal(innerDbs[0], 'folder1');
-      assert.equal(innerDbs[1], 'folder2');
-    })(done);
+        innerDbs = yield db.dbs();
+        assert.equal(innerDbs.length, 2);
+        assert.equal(innerDbs[0], 'folder1');
+        assert.equal(innerDbs[1], 'folder2');
+      })(done);
+    });
+
   });
 
 
-  it('db.db(key) returns nested db', function (done) {
-    co(function *() {
-      var db = yield docdb('test'),
-          innerDb = db.db('nested-db'),
-          path;
+  describe('.db(path)', function () {
 
-       path = yield innerDb.path();
-       assert.equal(path.split('/').slice(-2).join('/'), 'test/nested-db');
-    })(done);
+    it('returns nested db', function (done) {
+      co(function *() {
+        var db = yield docdb('test'),
+            innerDb = db.db('nested-db'),
+            path;
+
+         path = yield innerDb.path();
+         assert.equal(path.split('/').slice(-2).join('/'),
+           'test/nested-db');
+      })(done);
+    });
+
+
+    it('throws if path does not exist', function (done) {
+      co(function *() {
+        try {
+          var db = yield docdb('not-exists');
+          done(new Error('Error not thrown'));
+        } catch (e) {
+          done();
+        }
+      })();
+    });
+
+
+    it('throws if path is not a directory', function (done) {
+      fs.createFileSync('test/nested-db/file');
+
+      co(function *() {
+        try {
+          var db = yield docdb('test/nested-db/file');
+          done(new Error('Error not thrown'));
+        } catch (e) {
+          done();
+        }
+      })();
+    });
+
   });
 
-
-  it('db(path) throws if path does not exist', function (done) {
-    co(function *() {
-      try {
-        var db = yield docdb('not-exists');
-        done(new Error('Error not thrown'));
-      } catch (e) {
-        done();
-      }
-    })();
-  });
-
-
-  it('db(path) throws if path is not a directory', function (done) {
-    fs.createFileSync('test/nested-db/file');
-
-    co(function *() {
-      try {
-        var db = yield docdb('test/nested-db/file');
-        done(new Error('Error not thrown'));
-      } catch (e) {
-        done();
-      }
-    })();
-  });
 
 
   afterEach(function () {
